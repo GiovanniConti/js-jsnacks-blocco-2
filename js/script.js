667ecc8163b9a57baf0c2b6f371f5aafcb5f7f9b
GiovanniConti <giovanni.conti@live.it>
2021-10-15 17:14:39 +0200

    Fixed if we don't know the shortest array

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 
-// let mioNumero = prompt("Inserisci un numero.");
+/***************************** ESERCIZIO 1 ***************************************/
+
+/*
+let mioNumero = prompt("Inserisci un numero.");
 
 
-// console.log(mioNumero)
+console.log(mioNumero)
 
 
-// if((mioNumero % 2) === 0){
-//     alert(mioNumero);
+if((mioNumero % 2) === 0){
+    alert(mioNumero);
     
-// } else{
-//     mioNumero++;
-//     alert(mioNumero);
-// } 
-
+} else{
+    mioNumero++;
+    alert(mioNumero);
+} 
+*/
 
 /***************************** ESERCIZIO 2 ***************************************/
 
@@ -21,18 +24,39 @@ const array2 = [5];
 
 let i=0;
 
-// ciclo per far aggiungere elementi dell'array1 all'array2 fino a quando non avranno la stessa lunghezza
-while(array2.length < array1.length){
+// controllo qual è l'array con minor elementi
 
-    console.log(array2[i])
-    // valore dell'array prima del push
-    console.log("array2 prima del push: ", array2)
+if (array2.length < array1.length){
+
+    // ciclo per far aggiungere elementi dell'array1 all'array2 fino a quando non avranno la stessa lunghezza
+    while(array2.length < array1.length){
+
+        console.log(array2[i])
+        // valore dell'array prima del push
+        console.log("array2 prima del push: ", array2)
 
 
-    array2.push(array1[i])
+        array2.push(array1[i])
 
-    console.log("array2 dopo il push: ", array2)
+        console.log("array2 dopo il push: ", array2)
 
-    // incremento la i per far aggiungere l'elemento successivo dell'array1 all'array2
-    i++
+        // incremento la i per far aggiungere l'elemento successivo dell'array1 all'array2
+        i++
+    }
+} else{
+    while(array1.length < array2.length){
+
+        console.log(array1[i])
+        // valore dell'array prima del push
+        console.log("array1 prima del push: ", array1)
+
+
+        array1.push(array2[i])
+
+        console.log("array2 dopo il push: ", array1)
+
+        // incremento la i per far aggiungere l'elemento successivo dell'array1 all'array2
+        i++
+    }
 }
+/***************************** ESERCIZIO 3 ***************************************/
