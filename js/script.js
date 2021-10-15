944918764da8e23b638bf133cd23e199512e20ae
GiovanniConti <giovanni.conti@live.it>
2021-10-15 16:42:42 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 
-const mioNumero = prompt("Inserisci un numero.");
-const numPiuUno = mioNumero++;
+let mioNumero = prompt("Inserisci un numero.");
+let numPiuUno = mioNumero++;
 
 console.log(mioNumero)
 console.log(numPiuUno)
+
 if((mioNumero % 2) === 0){
     alert(`${mioNumero}`);
     
